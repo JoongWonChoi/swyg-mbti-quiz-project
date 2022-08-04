5924dda27fd30982cad16657a8e3419769665492
JoongWonChoi <qweasd1130@naver.com>
2022-08-04 15:20:56 +0900

    Change MYSQL Connection config

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -8,7 +8,7 @@ export const databaseProviders = [
         useFactory: async () =>{
             const dataSource = new DataSource({
                 type: 'mysql',
-                host: 'localhost',
+                host: '127.0.0.1',
                 port: 3306,
                 username: 'root',
                 password: '1234',
